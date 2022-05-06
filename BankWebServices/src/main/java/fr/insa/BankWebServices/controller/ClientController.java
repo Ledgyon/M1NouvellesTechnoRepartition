a159e2b6ffff0e01b03707068c77b164247317d6
Simon Rudent <Ledgyon>
2022-05-07 00:10:01 +0200

    changement ClientController maintenant RESTful

--- BankWebServices/src/main/java/fr/insa/BankWebServices/controller/ClientController.java
+++ BankWebServices/src/main/java/fr/insa/BankWebServices/controller/ClientController.java
@@ -1,7 +1,13 @@
 package fr.insa.BankWebServices.controller;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,42 +18,53 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fr.insa.BankWebServices.exception.ClientNotFoundException;
 import fr.insa.BankWebServices.models.Client;
+import fr.insa.BankWebServices.models.ClientModelAssembler;
 import fr.insa.BankWebServices.repository.ClientRepository;
 
 @RestController
-class ClientController {
+public class ClientController {
 
   private final ClientRepository repository;
+  
+  private final ClientModelAssembler assembler;
 
-  ClientController(ClientRepository repository) {
+  ClientController(ClientRepository repository, ClientModelAssembler assembler) {
     this.repository = repository;
+    this.assembler = assembler;
   }
 
 
   // Aggregate root
   // tag::get-aggregate-root[]
-  @GetMapping("/client")
-  List<Client> all() {
-    return repository.findAll();
+  @GetMapping("/employees")
+  public CollectionModel<EntityModel<Client>> all() {
+
+	  List<EntityModel<Client>> clients = repository.findAll().stream() //
+		      .map(assembler::toModel) //
+		      .collect(Collectors.toList());
+
+	  return CollectionModel.of(clients, linkTo(methodOn(ClientController.class).all()).withSelfRel());
   }
   // end::get-aggregate-root[]
 
   @PostMapping("/client")
-  Client newClient(@RequestBody Client newClient) {
+  public Client newClient(@RequestBody Client newClient) {
     return repository.save(newClient);
   }
 
   // Single item
   
   @GetMapping("/client/{id}")
-  Client one(@PathVariable Long id) {
-    
-    return repository.findById(id)
-      .orElseThrow(() -> new ClientNotFoundException(id));
+  public EntityModel<Client> one(@PathVariable Long id) {
+
+    Client client = repository.findById(id) //
+        .orElseThrow(() -> new ClientNotFoundException(id));
+
+    return assembler.toModel(client);
   }
 
   @PutMapping("/client/{id}")
-  Client replaceEmployee(@RequestBody Client newClient, @PathVariable Long id) {
+  public Client replaceEmployee(@RequestBody Client newClient, @PathVariable Long id) {
     
     return repository.findById(id)
       .map(client -> {
@@ -63,7 +80,7 @@ class ClientController {
   }
 
   @DeleteMapping("/client/{id}")
-  void deleteClient(@PathVariable Long id) {
+  public void deleteClient(@PathVariable Long id) {
     repository.deleteById(id);
   }
 }
