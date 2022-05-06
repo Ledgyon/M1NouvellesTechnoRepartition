5938c0599fc766c19e85796be3b69d8dff7bb8cd
Simon Rudent <Ledgyon>
2022-05-06 17:47:57 +0200

    init BankWebServices project

---
+++ BankWebServices/src/main/java/fr/insa/BankWebServices/BankWebServicesApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.BankWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebServicesApplication.class, args);
+	}
+
+}
