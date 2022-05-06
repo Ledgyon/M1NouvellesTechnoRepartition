ba063234cc47fcc33d95d189d5db3a8044b5d1f1
Simon Rudent <Ledgyon>
2022-05-06 18:08:40 +0200

    init Client

---
+++ BankWebServices/src/main/java/fr/insa/BankWebServices/models/Client.java
@@ -0,0 +1,76 @@
+package fr.insa.BankWebServices.models;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+	private @Id @GeneratedValue Long id;
+	private String name;	
+	private int age;	
+	private double solde;
+	
+	public Client() {}
+	
+	public Client(Long id, String name, int age, double solde) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.solde = solde;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSolde() {
+		return solde;
+	}
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, id, name, solde);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return age == other.age && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(solde) == Double.doubleToLongBits(other.solde);
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", age=" + age + ", solde=" + solde + "]";
+	}
+	
+	
+}
