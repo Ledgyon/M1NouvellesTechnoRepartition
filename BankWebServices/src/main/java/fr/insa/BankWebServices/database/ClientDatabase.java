ac0d8ac703982c0503c303910961515804938328
Simon Rudent <Ledgyon>
2022-05-07 17:03:28 +0200

    init PaymentGateway project

--- BankWebServices/src/main/java/fr/insa/BankWebServices/database/ClientDatabase.java
+++ BankWebServices/src/main/java/fr/insa/BankWebServices/database/ClientDatabase.java
@@ -10,16 +10,19 @@ import fr.insa.BankWebServices.models.Client;
 import fr.insa.BankWebServices.repository.ClientRepository;
 
 @Configuration
-class ClientDatabase {
+class LoadDatabase {
 
-  private static final Logger log = LoggerFactory.getLogger(ClientDatabase.class);
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(ClientRepository repository) {
+  CommandLineRunner initDatabase(ClientRepository employeeRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Client("Simon Rudent", 21, 1000)));
-      log.info("Preloading " + repository.save(new Client("Norman Di Bello", 21, 999)));
+      employeeRepository.save(new Client("Simon Rudent", 21, 1000));
+      employeeRepository.save(new Client("Norman Di Bello", 21, 999));
+
+      employeeRepository.findAll().forEach(client -> log.info("Preloaded " + client));
+      
     };
   }
 }
---
+++ PaymentGateway/src/test/java/fr/insa/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.PaymentGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
