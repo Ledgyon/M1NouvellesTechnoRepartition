024bac0d0a699d6f024332a31a24639cd77e37aa
Simon Rudent <Ledgyon>
2022-05-07 23:18:45 +0200

    init PaymentRepository et PaymentEndpoint

---
+++ PaymentGateway/src/main/java/fr/insa/PaymentGateway/producingwebservices/PaymentRepository.java
@@ -0,0 +1,46 @@
+package fr.insa.PaymentGateway.producingwebservices;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import io.spring.payment.Payment;
+import io.spring.payment.Type;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class PaymentRepository {
+	private static final Map<Integer, Payment> payments = new HashMap<>();
+
+	@PostConstruct
+	public void initData() {
+		Payment op1 = new Payment();
+		op1.setIdOperation(1);
+		op1.setPrix(200);
+		op1.setType(Type.DEBIT);
+
+		payments.put(op1.getIdOperation(), op1);
+
+		Payment op2 = new Payment();
+		op1.setIdOperation(2);
+		op1.setPrix(500);
+		op1.setType(Type.REMBOURSEMENT);
+
+		payments.put(op2.getIdOperation(), op2);
+
+		Payment op3 = new Payment();
+		op1.setIdOperation(3);
+		op1.setPrix(400);
+		op1.setType(Type.DEBIT);
+
+		payments.put(op3.getIdOperation(), op3);
+	}
+
+	public Payment findPayment(Integer idOperation) {
+		Assert.notNull(idOperation, "The idOperation must not be null");
+		return payments.get(idOperation);
+	}
+}
---
+++ PaymentGateway/src/main/java/fr/insa/PaymentGateway/producingwebservices/endpoint/PaymentEndpoint.java
@@ -0,0 +1,32 @@
+package fr.insa.PaymentGateway.producingwebservices.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import fr.insa.PaymentGateway.producingwebservices.PaymentRepository;
+import io.spring.payment.GetIdOperationRequest;
+import io.spring.payment.GetIdOperationResponse;
+
+@Endpoint
+public class PaymentEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/payment";
+
+	private PaymentRepository paymentRepository;
+
+	@Autowired
+	public PaymentEndpoint(PaymentRepository paymentRepository) {
+		this.paymentRepository = paymentRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPaymentRequest")
+	@ResponsePayload
+	public GetIdOperationResponse getCountry(@RequestPayload GetIdOperationRequest request) {
+		GetIdOperationResponse response = new GetIdOperationResponse();
+		response.setPayment(paymentRepository.findPayment(request.getIdOperation()));
+
+		return response;
+	}
+}
